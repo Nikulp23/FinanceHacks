307f39aa64457535c50e85c363293e61d0548512
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-02-17 16:17:21 -0500

    Rounds distance to 2 decimals

--- backend/routes/getBanks.js
+++ backend/routes/getBanks.js
@@ -46,7 +46,8 @@ async function fetchDistance(startCoords, endCoords) {
 
   const distance = await response.json();
   
-  const dist = distance.resourceSets[0].resources[0].results[0].travelDistance;
+  let dist = distance.resourceSets[0].resources[0].results[0].travelDistance;
+  dist = Math.round(dist * 100) / 100
   const time = distance.resourceSets[0].resources[0].results[0].travelDuration;
   return [dist, time];
   
