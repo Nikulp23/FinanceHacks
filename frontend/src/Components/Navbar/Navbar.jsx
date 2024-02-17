a6239cf39fe2a6dce097bc7387625245beb010cd
Robert Acebedo <robace747@gmail.com>
2024-02-17 11:57:04 -0500

    GSLIRYGASLIHKJGM;

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import img from '../../assets/CartX-logos/logo-transparent-png.png'
 import './Navbar.css'
 
 const NavBar = () => {
@@ -10,15 +9,14 @@ const NavBar = () => {
    <div className="header">
       <Navbar className="navbar">
         <Container>
-          <Navbar.Brand href="#home">
-            <img className="logo" src={ img } alt="CartX Logo" width="60"height="60"/>
+          <h1>hi</h1>
+          {/* <Navbar.Brand href="#home">
           </Navbar.Brand>
-          <SearchBar className="searchbar"/>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          </Nav> */}
         </Container>
       </Navbar>
    </div>
