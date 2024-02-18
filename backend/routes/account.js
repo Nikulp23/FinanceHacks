d3089b5989819b10c080a600c0ea046493661040
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-02-18 00:33:38 -0500

    Working in progress accounts

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -40,7 +40,7 @@ router.post(`/${parsed.name}`, async (req, res) => {
 
    Promise.all(req.body.banks.map(bank => generateBankDetails(bank, CITIZENSHIP_STATUS, CURRENT_AGE, WORKING_STATUS))).then(results => {
 
-      // console.log(results);
+      console.log(results);
 
       const jsonObjectsArray = results.reduce((acc, jsonString) => {
          try {
