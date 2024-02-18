7cad11da45bb15fd6235d6d325103d436331458d
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-02-17 19:00:46 -0500

    Updated json format

--- frontend/src/Components/JsonFormats/JsonFormat.jsx
+++ frontend/src/Components/JsonFormats/JsonFormat.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import OpenAccountFormat from './formats/OpenAccountFormat.jsx';
+// import ApplyFormat from './formats/ApplyFormat.jsx';
 
-const DynamicComponentSelector = ({ data, selectedOption }) => {
+const JsonFormat = ({ data, selectedOption }) => {
    // Here, you decide which component to render based on selectedOption
    console.log(data)
-   console.log(data.banks)
    if(data == undefined || data.error == "Unknown Request"){
     return <div>Unable to access backend data. Please try again.</div>
+   }
+   else if (data.error == "Unknown Request") {
+    return <div>Error processing data.</div>
    }
    if (selectedOption === 'openAccount' && data.banks != undefined) {
      return <OpenAccountFormat banks={data.banks} />;
    } 
    else if (selectedOption === 'applyLoan') {
+    return <div>{data}</div>
     return <div>Apply loan</div>
    }
    else if (selectedOption === 'selectCreditCard') {
@@ -22,4 +26,4 @@ const DynamicComponentSelector = ({ data, selectedOption }) => {
    }
  };
  
- export default DynamicComponentSelector;
+ export default JsonFormat;
