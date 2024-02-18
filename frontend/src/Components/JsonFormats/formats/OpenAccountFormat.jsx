dc177d179719429b93adc956d8be895faa8175ae
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-02-18 00:39:34 -0500

    Fixed formatting

--- frontend/src/Components/JsonFormats/formats/OpenAccountFormat.jsx
+++ frontend/src/Components/JsonFormats/formats/OpenAccountFormat.jsx
@@ -21,16 +21,16 @@ const OpenAccountFormat = ({ banks }) => {
                   <p><strong>Address:</strong> {bank.address}</p>
 
                   <h3>Account Opening Requirements:</h3>
-                  <p><strong>Identification:</strong> {bank.account_opening_requirements.identification}</p>
-                  <p><strong>Proof of Address:</strong> {bank.account_opening_requirements.proof_of_address}</p>
+                  <p><strong>Identification:</strong> {bank.account_opening_requirements?.identification}</p>
+                  <p><strong>Proof of Address:</strong> {bank.account_opening_requirements?.proof_of_address}</p>
                   {bank.account_opening_requirements.additional_documents[0] !== "None" &&
-                    <p><strong>Additional Documents:</strong> {bank.account_opening_requirements.additional_documents}</p>
+                    <p><strong>Additional Documents:</strong> {bank.account_opening_requirements?.additional_documents}</p>
                   }
 
                   <h3>Types of Accounts:</h3>
-                  <p><strong>Checking Account:</strong> {bank.types_of_accounts.checking_account}</p>
-                  <p><strong>Savings Account:</strong> {bank.types_of_accounts.savings_account}</p>
-                  <p><strong>Money Market Account:</strong> {bank.types_of_accounts.money_market_account}</p>
+                  <p><strong>Checking Account:</strong> {bank.types_of_accounts?.checking_account}</p>
+                  <p><strong>Savings Account:</strong> {bank.types_of_accounts?.savings_account}</p>
+                  <p><strong>Money Market Account:</strong> {bank.types_of_accounts?.money_market_account}</p>
 
                   <h3>Considerations:</h3>
                   <p><strong>Fees:</strong> {bank.considerations.fees}</p>
